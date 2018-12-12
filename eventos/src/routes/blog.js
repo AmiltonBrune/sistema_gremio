19e33a3de688206a2cfdc89b922960a430ac729e
Amilton Brune <amiltonbrune.22@gmail.com>
2018-12-12 01:00:36 -0200

    Alteração na pagina do blog

--- eventos/src/routes/blog.js
+++ eventos/src/routes/blog.js
@@ -3,14 +3,13 @@ const router = express.Router();
 const db = require('../db');
 
 /** GET forgot */
-router.get('/', global.authenticationMiddleware(), function(req, res, next){
-  var nome = req.user.nome;
-  var sobrenome = req.user.sobrenome;
+router.get('/', function(req, res, next){
+  
   db.findAll((e, docs) => {
       if(e) { return console.log(e); }
       db.todosPosts((e, doc)=>{
         if(e) { return console.log(e); }
-        res.render('blog', { title: 'Tela Inicial', docs: docs, nome: nome, sobrenome: sobrenome,doc: doc });
+        res.render('blog', { title: 'Blog', docs: docs,doc: doc });
       })
   })
   });
